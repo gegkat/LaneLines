fe9cee609cd497de66b155c7aada5105e7955afa
Gregory Katz <gkatz@fb.com>
2017-03-12 20:43:01 -0700

    add curvature

--- lanes.py
+++ lanes.py
@@ -338,9 +338,26 @@ def fitpoly(fname, mtx, dist, i):
   left_fit = np.polyfit(lefty, leftx, 2)
   right_fit = np.polyfit(righty, rightx, 2)
 
-  plot_process(undist, warped, Minv, binary_warped, left_fit, right_fit, out_img, i)
+  left_curved, right_curved = plot_process(undist, warped, Minv, binary_warped, left_fit, right_fit, out_img, i)
 
   return left_fit, right_fit
+
+def curvature(ploty, leftx, rightx, yeval):
+  # Define conversions in x and y from pixels space to meters
+  ym_per_pix = 30/720 # meters per pixel in y dimension
+  xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+  y_eval = np.max(ploty)
+
+  # Fit new polynomials to x,y in world space
+  left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+  right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+  # Calculate the new radii of curvature
+  left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+  right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+  # Now our radius of curvature is in meters
+  print(left_curverad, 'm', right_curverad, 'm')
+  return left_curverad, right_curverad
 
 def plot_process(undist, warped, Minv ,binary_warped, left_fit, right_fit, out_img, i):
   f,axs = plt.subplots(2,3)
@@ -349,6 +366,7 @@ def plot_process(undist, warped, Minv ,binary_warped, left_fit, right_fit, out_i
   axs[0,2].imshow(binary_warped, cmap='gray')
 
   ploty, left_fitx, right_fitx = get_rect_plots(binary_warped, left_fit, right_fit)
+  left_curved, right_curved = curvature(ploty, left_fitx, right_fitx, ploty)
   plot_rect_method(axs[1,0], ploty, left_fitx, right_fitx, out_img)
 
   #plt.xlim(0, 1280)
@@ -360,6 +378,7 @@ def plot_process(undist, warped, Minv ,binary_warped, left_fit, right_fit, out_i
   print(savename)
   plt.savefig(savename)
   plt.close()
+  return left_curved, right_curved
 
 def get_rect_plots(binary_warped, left_fit, right_fit):
   ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
